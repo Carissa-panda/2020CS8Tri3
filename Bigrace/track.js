da929236dd17c9d12c3267dbe37ce2b134df8c98
Carissa-panda <cschultz24@nscds.org>
2020-03-10 16:18:23 +0000

    saved track.js

--- Bigrace/track.js
+++ Bigrace/track.js
@@ -13,10 +13,9 @@ function startRace(){
     TweenMax.to("#car1",2,{x:520,ease:Sine.easeOut});
     TweenMax.to("#car1",1.5,{x:500, y:-320,delay:1.7, ease:Sine.easeOut});
     TweenMax.to("#car1",2,{x:-50, y:-300,delay:3, ease:Sine.easeOut});
-
 }
     
-   TweenMax.to("#car1",1.5,{x:50, y:100,delay:1.7, ease:Sine.easeOut});
+   
 
       
     
